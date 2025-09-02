090acdc1448634897598edef84fb5af3da89d0bf
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-03 00:39:23 +0430

    we add the css external

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
-
+import "./Header.css"
 
 function Header() {
     return (
         <div>
-            <h1>this is header </h1>
+            <h1 className="header">this is header </h1>
 
-            <ul style={{listStyleType: "none" , display: "flex", justifyContent: "center", gap: "20px", backgroundColor: "gray"}}>
+            {/* <ul style={{listStyleType: "none" , display: "flex", justifyContent: "center", gap: "20px", backgroundColor: "gray"}}> */}
+               <ul>
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
