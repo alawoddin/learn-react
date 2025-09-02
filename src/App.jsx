23c39200209c33bf85b3ccec38aacf7f7047068e
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-03 00:23:10 +0430

    we create the component floder is create

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      {/* <Navber /> */}
+      {/* <Header />
+      <Footer /> */}
+      {/* <h1>hello developer</h1> */}
+    </div>
+  );
+}
+
+// const Navber = () => {
+//   return (
+//     <div>
+//       <h1>this is for demo</h1>
+//     </div>
+//   )
+// } 
+// function Navber() {
+//   return (
+//     <div>
+//       <h1>this is for demo</h1>
+//     </div>
+//   )
+// }
+
+// function Header() {
+//   return (
+//     <ul>
+//       <li>Home</li>
+//       <li>About</li>
+//       <li>Contact</li>
+//     </ul>
+//   )
+// }
+
+// const Footer = () => {
+//   return (
+//     <div>
+//       <h1> this is for footer</h1>
+//     </div>
+//   )
+// }
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+    return (
+        <div>
+            <h1>this is header </h1>
+
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default Header;
