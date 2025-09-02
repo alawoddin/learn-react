d7335f82c42ac19410068afb6eeccef48be5e005
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-03 00:07:05 +0430

    create the header and footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 function App() {
   return (
     <div className="App">
-      <h1>hello developer</h1>
+      <Navber />
+      <Header />
+      <Footer />
+      {/* <h1>hello developer</h1> */}
     </div>
   );
+}
+
+// const Navber = () => {
+//   return (
+//     <div>
+//       <h1>this is for demo</h1>
+//     </div>
+//   )
+// } 
+function Navber() {
+  return (
+    <div>
+      <h1>this is for demo</h1>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      <h1> this is for footer</h1>
+    </div>
+  )
 }
 
 export default App;
