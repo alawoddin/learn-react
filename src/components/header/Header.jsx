78ac359ffa31212680dd4b0d21ac6bd1a21913cc
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-04 22:43:15 +0430

    the logo section is add

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,14 @@ import styled from './Header.module.css'
 
 
 function Header() {
+
+        let title = 'logo';
+
     return (
+
         <div className={styled.header}>
+
+            <h1>{title}</h1>
 
             {/* <ul style={{listStyleType: "none" , display: "flex", justifyContent: "center", gap: "20px", backgroundColor: "gray"}}> */}
                <ul>
