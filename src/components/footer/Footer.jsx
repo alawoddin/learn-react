15c9bfcfc59e4067fc899b266139684daa9ec951
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-04 21:55:50 +0430

    the inline style is add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <div>
-            <h1>
+            <h1 style={{backgroundColor: "green" }}>
                 this is for footer
             </h1>
         </div>
