7df28badccaed83c058faf22f3de40077b3cf8d6
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2025-09-05 00:19:13 +0430

    the main section actricture is done

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,11 @@
+import styled from './article.module.css';
+
+
+function Article() {
+    return (
+        <div className={styled.articleWrapper}>
+        </div>
+    )
+}
+
+export default Article;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,27 @@
+import Article from "../../components/article/Article";
 import Header from "../../components/header/Header";
+import styled from './home.module.css';
 
 
 function Home ()  {
     return (
-        <div>
+        <div className={styled.homeWrapper}>
             <Header title= 'develoepr'/>
-            <div>
-                <h1 className="container" style={{textAlign: "right"}}>home </h1>
+            <div className="container">
+                <h2>home</h2>
+
+                <div className={styled.articles}>
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+
+                </div>
+
             </div>
         </div>
     )
